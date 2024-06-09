d27e88a08cba1d20757b4400d88e66a0ff27cebe
Aijaz Ansari <aijaz@users.noreply.github.com>
2024-06-09 12:15:25 -0600

    Update aid.sh to final, production version
    Added comments and prompts

--- aid.sh
+++ aid.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "This is a script that handles financial aid assistance forms"
+
 echo -n "Enter starting bank balance: "
 
 # read input and save it into the variable named balance
@@ -7,10 +9,15 @@ read balance
 
 balance=$((balance + 0))
 
+echo "Starting balance is $balance"
+
+echo ""
+
 while [ "$balance" -gt "0" ]; do
+    echo "Enter aid amount (Balance is $balance): "
     read amt
     balance=$((balance - amt))
 done
 
-echo "Done"
+echo "Ending the program - balance is $balance"
 
